8273db7140ddefaf97690e9cc6d404e966656713
Carlos <carlosscripto@gmail.com>
2024-11-25 23:18:59 -0500

    refactor monohilo.js to enhance asynchronous behavior demonstration and improve error handling

--- fundamentos-node-platzi-master/conceptos/monohilo.js
+++ fundamentos-node-platzi-master/conceptos/monohilo.js
@@ -1,35 +1,27 @@
-// console.log('Hola mundo');
-// let i = 0;
-// setInterval(function() {
-//     console.log(i);
-//     i++;
-
-//     // if (i === 5) {
-//     //     console.log('forzamos error');
-//     //     var a = 3 + z;
-//     // }
-// }, 1000);
-
-// console.log('Segunda instrucción');
-
-console.log('Hola mundo');
+console.log('> Nodejs es asincrono a pesar de ser monohilo');
 
 let i = 0;
 const intervalID = setInterval(() => {
+    if(i === 0) {
+        console.log("> () => Inicia el callback de setInterval")
+        console.log('> La segunda instrucción se ejecuta antes de este callback');
+    };
     console.log(i);
     i++;
     if (i === 5) {
-        // console.log('forzamos error');
         try {
-            let a = 3 + z;
+            console.log('> forzamos error');
+            let a = 3 + variableNoDefinida;
         } catch (error) {
-            console.error('Ha ocurrido un error');
+            console.error('> Ha ocurrido un error: ', error.message);
             // process.exit(1);
         }
     }
     if(i === 10) {
-        console.log('Terminamos el proceso');
+        console.log('> Terminamos el proceso');
         clearInterval(intervalID);
         // process.exit(0);
     }
 }, 1000);
+
+console.log('> Segunda instrucción!!');
