32d2fa2a02a8f0a37e06421d88b09161666e8b2f
Carlos <carlosscripto@gmail.com>
2024-11-25 23:11:00 -0500

    refactor monohilo.js to improve error handling and process termination

--- fundamentos-node-platzi-master/conceptos/monohilo.js
+++ fundamentos-node-platzi-master/conceptos/monohilo.js
@@ -1,13 +1,35 @@
+// console.log('Hola mundo');
+// let i = 0;
+// setInterval(function() {
+//     console.log(i);
+//     i++;
+
+//     // if (i === 5) {
+//     //     console.log('forzamos error');
+//     //     var a = 3 + z;
+//     // }
+// }, 1000);
+
+// console.log('Segunda instrucción');
+
 console.log('Hola mundo');
+
 let i = 0;
-setInterval(function() {
+const intervalID = setInterval(() => {
     console.log(i);
     i++;
-
-    // if (i === 5) {
-    //     console.log('forzamos error');
-    //     var a = 3 + z;
-    // }
+    if (i === 5) {
+        // console.log('forzamos error');
+        try {
+            let a = 3 + z;
+        } catch (error) {
+            console.error('Ha ocurrido un error');
+            // process.exit(1);
+        }
+    }
+    if(i === 10) {
+        console.log('Terminamos el proceso');
+        clearInterval(intervalID);
+        // process.exit(0);
+    }
 }, 1000);
-
-console.log('Segunda instrucción');
